617fed08b7085e3d24b90324c23f2a7e37addb86
Pavel <duminpn@gmail.com>
2023-01-04 19:51:09 +0300

    some updates

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 
 st.header('QA & cheatsheets Elbrus Data Science')
+
+
+
+
 
--- pages/10_PairGenerator.py
+++ pages/10_PairGenerator.py
@@ -5,17 +5,18 @@ with st.form('teams_generator'):
     names = st.radio(
                 'Choose a team', 
                 [
-                    'Дмитрий, Валентина, Иван, Андрей, Алексей, Диана, Санчай, Варвара',
-                    'Гульназ, Пётр, Александр, Александра, Анна, Никита, Ольга'
+                    # 'Дмитрий, Валентина, Иван, Андрей, Алексей, Диана, Санчай, Варвара',
+                    # 'Гульназ, Пётр, Александр, Александра, Анна, Никита, Ольга'
+                    'Сиражудин, Мила, Семен, Анатолий, Гор, Матвей'
                     # add here more names as str
                 ]
             )
-    team_names = st.text_input('Teams', placeholder='Logistic Regression, Gradient Boosting, Random Forest, Support Vector Machine')
+    team_names = st.text_input('Teams', placeholder='Convolutional, Dropout, Linear, BatchNormalization')
 
     st.form_submit_button('Generate teams!', on_click=random_people_choice, args=(names.split(','), team_names.split(',')))   
 
 
 if names and team_names:
-    pairs = random_people_choice(names.split(), team_names.split(','))
+    pairs = random_people_choice(names.split(','), team_names.split(','))
     for team_name, names in pairs.items():
-        st.write(f'{team_name}: {(" ".join(names))}')
+        st.write(f'{team_name}: {(", ".join(names))}')
